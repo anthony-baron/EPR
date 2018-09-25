4568b641ffd92907f0c9393bf03796b9866c9c69
Anthony Baron <anthony.baron@myacuity.com>
2018-09-25 10:03:06 -0700

    Adding classes

---
+++ EPR/src/app/classes/pet.ts
@@ -0,0 +1,13 @@
+export class Pet {
+
+    Pet_ID: number;
+    PetType_ID: number;
+    Name: string;
+    Color: string;
+    Weight: number;
+    Description: string;
+    HealthInfo: string;
+    Location: string;
+    Rescued: boolean;
+    
+}
---
+++ EPR/src/app/classes/user.ts
@@ -0,0 +1,19 @@
+export class User{
+
+    USER_ID: number;
+    USER_NAME: string;
+    FirstName: string;
+    LastName: string;
+    Email: string;
+    PhoneNumber: string;
+    Active: boolean;
+    Address: string;
+    City: string;
+    State: string;
+    ZipCode: number;
+    Latitude: string;
+    Longitude: string;
+    Pet_ID: number;
+    UserType_ID: number;
+    
+}
