95ef1e130c09ab6453e8da0d95586302e875bfa9
Anthony Baron <anthony.baron@myacuity.com>
2018-11-02 17:50:51 -0700

    Adding mock endpoint for get all users

--- EPR/src/app/services/user.service.ts
+++ EPR/src/app/services/user.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+//import user class to create observables
+import { User } from '../classes/user'
+import { Observable } from '../../../node_modules/rxjs';
+import { catchError } from '../../../node_modules/rxjs/operators'; 
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class UserService {
 
-  constructor() { }
+
+
+  constructor(private http: HttpClient) { }
+ // const apiHeaders
+ httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
+
+ public baseApiURL: string = environment.baseApiUrl;
+
+ //get all users - base + swap with real endpoint later
+ public getAllUsers(): Observable<User[]>{
+   return this.http.get<User[]>(this.baseApiURL + 'dbo_user.json/mockUsers?key=937c3910', this.httpOptions)
+  
+ }
+ public getBaseEndpoint(){
+   
+ }
 }
